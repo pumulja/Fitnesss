c1759954ddbaf677c312a0064d795f25ae1d71cb
rita <pumulja@mail.ru>
2018-04-02 23:42:38 +0300

    itog

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ my_button.onclick = function() {
 
 	const name = document.getElementById('name');
 	const email = document.getElementById('email');
-	const form = document.getElementById('email');
+	const form = document.getElementById('contact_form');
+	const message = document.getElementById('message_block');
 
 	let classNames = name.classList;
 	let classNamesEmail = email.classList;
 	let classNamesForm = form.classList;
+	let classNamesMessage = message.classList;
+
 
 	console.log(classNamesForm);
 
@@ -28,6 +31,16 @@ my_button.onclick = function() {
 		classNamesEmail.add("red");
 		console.log(email.classList);
 	}
+	else {
+		classNamesEmail.remove("red");
+		console.log(email.classList);
+	}
 
+	if (name.value != "" && email.value != "") {
+		form.style.display = "none";
+		// classNamesForm.remove("form");
+		message.style.display = "block";
+		console.log(message.classList);
 
+	}
 }
